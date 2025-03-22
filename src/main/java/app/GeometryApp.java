f765c93485d3b12da62d86792220d0db9fda36d2
User2 <vkagarmanova@inbox.ru>
2025-03-22 20:01:31 +0500

    Добавлена зависимость "ThreeDimensionalShapes" + обновление версии до "1.1.0-SNAPSHOT"

--- src/main/java/app/GeometryApp.java
+++ src/main/java/app/GeometryApp.java
@@ -4,6 +4,8 @@ import shapes.Circle;
 import shapes.Rectangle;
 import shapes.Triangle;
 import utils.GeometryUtils;
+import threedim.Cube;
+import threedim.Sphere;
 
 
 public class GeometryApp {
@@ -13,11 +15,19 @@ public class GeometryApp {
         Circle circle = new Circle(10);
         Rectangle rectangle = new Rectangle(8, 5);
         Triangle triangle = new Triangle(6, 7, 8);
+        Cube cube = new Cube(5);
+        Sphere sphere = new Sphere(3);
 
         printInfo("Круг", circle.getArea(), circle.getPerimeter(), circle.getDiameter());
         printInfo("Прямоугольник", rectangle.getArea(), rectangle.getPerimeter());
         printInfo("Треугольник", triangle.getArea(), triangle.getPerimeter());
         printCm2ToM2(1000.00);
+        printCompareAreas(200, 200);
+        printInfo("Куб", cube.getSurfaceArea());
+        printInfo("Сфера", sphere.getSurfaceArea());
+
+        cube.toString();
+        sphere.toString();
 
     }
 
@@ -39,5 +49,24 @@ public class GeometryApp {
     private static void printCm2ToM2(double cm2) {
         System.out.println(cm2 + " см кв."  + " = " + GeometryUtils.cm2ToM2(cm2) + " м кв.");
         System.out.println("----------------------------");
+    }
+
+    private static void printInfo (String figure, double area) {
+        System.out.println("Фигура: " + figure);
+        System.out.printf("  Площадь: %.2f\n", area);
+        System.out.println("----------------------------");
+    }
+
+    private static void printCompareAreas(double area1, double area2){
+        if (GeometryUtils.compareAreas(area1, area2) == -1){
+            System.out.println("Площадь первой фигуры меньше площади второй фигуры");
+            System.out.println("----------------------------");
+        }else if(GeometryUtils.compareAreas(area1, area2) == 1){
+            System.out.println("Площадь первой фигуры больше площади второй фигуры");
+            System.out.println("----------------------------");
+        }else {
+            System.out.println("Площадь фигур равны");
+            System.out.println("----------------------------");
+        }
     }
 }
